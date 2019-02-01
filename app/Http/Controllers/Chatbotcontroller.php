47ca88c33e405acbaa06ae00cb6abc5e52317529
Juliot Nuñez <nunezjuliot@gmail.com>
2019-02-01 12:57:19 -0400

    test Chatbotcontroller index

--- app/Http/Controllers/Chatbotcontroller.php
+++ app/Http/Controllers/Chatbotcontroller.php
@@ -8,6 +8,9 @@ class Chatbotcontroller extends Controller
 {
     public function index()
     {
-    	
+    	$feeddata = file_get_contents(filename: 'php://inpunt');
+    	$handle = fopen(filename:'test.txt', mode:'w');
+    	fwrite($handle, $feeddata);
+    	fclose($handle);
     }
 }
